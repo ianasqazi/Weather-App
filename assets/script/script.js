339daede000155e5849ad6e7213863ec0cb6eb6b
Anas Qazi <ianasqazi@gmail.com>
2019-10-31 04:32:18 -0400

    logic to forcast working

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,6 +16,8 @@ function callAPI(){
   var currentCity = $.trim($("#searchCity").val());
 
   var APIKey = "b72c0d35aba9f0b8c0e9ebb9ec68c3f8";
+
+  // First API Call to get current City Weather
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=metric&appid=" + APIKey;
   
   $.ajax({
@@ -32,18 +34,21 @@ function callAPI(){
     $("#currentHumidity").text("Humidity : " + response.main.humidity + " %");
     $("#currentWindSpeed").text("Wind Speed : " + response.wind.speed + " m/s");
 
+
+  // Second API Call to get UV Index Value by using values from the response of First API call
+
       var latValue=(response.coord.lat);
       var lonValue=response.coord.lon;
-      var queryUrlUV="http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+ latValue + "&lon=" + lonValue;
+      
+      var queryURLUV="http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+ latValue + "&lon=" + lonValue;
         $.ajax({
-          url: queryUrlUV,
+          url: queryURLUV,
           method: "GET"
           }).then(function(response) {  
 
           $("#uvValue").text(response.value);
           
           var checkIndex=Math.round(response.value);
-          console.log(checkIndex);
           if(checkIndex>=11){
             $("#uvValue").attr("class","fuschia");
           }
@@ -62,6 +67,37 @@ function callAPI(){
 
         });
 
+    // Third API call to get forcast using values from response 1st API call
+        var cityID=response.id;
+        var queryURLForcast="http://api.openweathermap.org/data/2.5/forecast?appid="+APIKey+"&units=metric&id="+cityID;
+        console.log(queryURLForcast);
+       
+        $.ajax({
+          url: queryURLForcast,
+          method: "GET"
+          }).then(function(response) { 
+            
+            $("#forcast").append("<div class=row id=forcastBlock></div>")
+            for(i=1,j=0; i<=5; i++){
+
+              $("#forcastBlock").append("<div class=col id=day"+i+"></div>");
+              $("#day"+i).append("<p id=forcastDate"+i+">p</p>");
+              $("#day"+i).append("<img id=iconForcast"+i+" "+"alt=weatherLogo></img>");
+              $("#day"+i).append("<p id=forcastTemp"+i+">p</p>");
+              $("#day"+i).append("<p id=forcastHumidity"+i+">p</p>");
+
+              $("#forcastDate"+i).text(response.list[j].dt_txt);
+
+              var iconCode=response.list[j].weather[0].icon;
+              var iconURL="http://openweathermap.org/img/w/"+iconCode+".png";
+              $("#iconForcast"+i).attr("src",iconURL);
+
+              $("#forcastTemp"+i).text("Temp : " + response.list[j].main.temp + " " + String.fromCharCode(176) + "C");
+              $("#forcastHumidity"+i).text("Humidity : " + response.list[j].main.humidity + " %");
+              j=j+8;
+
+            }
+        });
   });
 
   }
@@ -71,7 +107,7 @@ function callAPI(){
 
 // 
 
-// if (angle >= 90 && angle <= 180) {
+
 
 /* Green	  Low	0 - 2
 Yellow	Moderate	3 - 5
